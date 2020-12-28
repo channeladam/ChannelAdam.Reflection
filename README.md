33174f0d1fad0388f94f05af6c6b8539e111586d
Adam <adammcraven@gmail.com>
2020-12-28 17:24:24 +1000

    3.0.0 - Multitarget .NET Standard 2.0 and .NET 5.0, enabled nullable reference types, breaking change to DeserialiseFromXmlResource() when using XmlAttributeOverrides.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ChannelAdam.Reflection
+
 A .NET library that provides helpers for using reflection.
+
+Targets:
+
+- .NET 5.0
+- .NET Standard 2.0
--- src/ChannelAdam.Reflection/EmbeddedResource.cs
+++ src/ChannelAdam.Reflection/EmbeddedResource.cs
@@ -1,6 +1,6 @@
 ﻿//-----------------------------------------------------------------------
 // <copyright file="EmbeddedResource.cs">
-//     Copyright (c) 2016-2018 Adam Craven. All rights reserved.
+//     Copyright (c) 2016-2021 Adam Craven. All rights reserved.
 // </copyright>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -58,22 +58,17 @@ namespace ChannelAdam.Reflection
         /// <returns>The embedded resource as a string.</returns>
         public static string GetAsString(Assembly assembly, string resourceName)
         {
-            Stream stream = null;
+            Stream? stream = null;
             try
             {
                 stream = GetAsStream(assembly, resourceName);
-                using (var reader = new StreamReader(stream))
-                {
-                    stream = null;
-                    return reader.ReadToEnd();
-                }
+                using var reader = new StreamReader(stream);
+                stream = null;
+                return reader.ReadToEnd();
             }
             finally
             {
-                if (stream != null)
-                {
-                    stream.Dispose();
-                }
+                stream?.Dispose();
             }
         }
 
@@ -95,12 +90,10 @@ namespace ChannelAdam.Reflection
         /// <param name="assembly">The assembly that is storing the embedded resource.</param>
         /// <param name="xmlResourceName">Name of the embedded XML resource.</param>
         /// <returns>The deserialised object.</returns>
-        public static T DeserialiseFromXmlResource<T>(Assembly assembly, string xmlResourceName)
+        public static T? DeserialiseFromXmlResource<T>(Assembly assembly, string xmlResourceName)
         {
-            using (var stream = GetAsStream(assembly, xmlResourceName))
-            {
-                return stream.DeserialiseFromXml<T>();
-            }
+            using var stream = GetAsStream(assembly, xmlResourceName);
+            return stream.DeserialiseFromXml<T>();
         }
 
         /// <summary>
@@ -111,12 +104,10 @@ namespace ChannelAdam.Reflection
         /// <param name="xmlResourceName">Name of the XML embedded resource.</param>
         /// <param name="xmlRootAttribute">XML root attribute override for deserialisation.</param>
         /// <returns>The deserialised object.</returns>
-        public static T DeserialiseFromXmlResource<T>(Assembly assembly, string xmlResourceName, XmlRootAttribute xmlRootAttribute)
+        public static T? DeserialiseFromXmlResource<T>(Assembly assembly, string xmlResourceName, XmlRootAttribute xmlRootAttribute)
         {
-            using (var stream = GetAsStream(assembly, xmlResourceName))
-            {
-                return stream.DeserialiseFromXml<T>(xmlRootAttribute);
-            }
+            using var stream = GetAsStream(assembly, xmlResourceName);
+            return stream.DeserialiseFromXml<T>(xmlRootAttribute);
         }
 
         /// <summary>
@@ -125,14 +116,32 @@ namespace ChannelAdam.Reflection
         /// <typeparam name="T">The type to deserialise the XML into.</typeparam>
         /// <param name="assembly">The assembly that is storing the embedded resource.</param>
         /// <param name="xmlResourceName">Name of the XML embedded resource.</param>
+        /// <param name="equalityKeyOfXmlAttributeOverridesToAvoidXmlSerializerMemoryLeak">Key of the XmlSerializer cache, unique for the given XmlAttributeOverrides, used to avoid XmlSerializer memory leaks. CAUTION: XmlAttributeOverrides.GetHashCode() returns a different value for each instance, even if each instance has the exact same objects - so consider making your own equality key based on what you added to the XmlAttributeOverrides.</param>
         /// <param name="xmlAttributeOverrides">XML attribute overrides for deserialisation.</param>
         /// <returns>The deserialised object.</returns>
-        public static T DeserialiseFromXmlResource<T>(Assembly assembly, string xmlResourceName, XmlAttributeOverrides xmlAttributeOverrides)
+        /// <remarks>
+        /// <para>
+        /// https://docs.microsoft.com/en-us/dotnet/api/system.xml.serialization.xmlserializer?view=net-5.0#dynamically-generated-assemblies
+        /// Dynamically Generated Assemblies
+        /// To increase performance, the XML serialization infrastructure dynamically generates assemblies to serialize and deserialize specified types.
+        /// The infrastructure finds and reuses those assemblies.
+        /// This behavior occurs only when using the following constructors:
+        ///   XmlSerializer.XmlSerializer(Type)
+        ///   XmlSerializer.XmlSerializer(Type, String)
+        /// If you use any of the other constructors, multiple versions of the same assembly are generated and never unloaded, which results in a memory leak and poor performance.
+        /// The easiest solution is to use one of the previously mentioned two constructors.
+        /// Otherwise, you must cache the assemblies...
+        /// </para>
+        /// <para>
+        /// ChannelAdam.Xml does the caching for you, but it requires you to specify the key to use in the cache.
+        /// CAUTION: XmlAttributeOverrides.GetHashCode() returns a different value for each instance, even if each instance has the exact same objects
+        ///   - so consider making your own equality key based on what you added to the XmlAttributeOverrides.
+        /// </para>
+        /// </remarks>
+        public static T? DeserialiseFromXmlResource<T>(Assembly assembly, string xmlResourceName, string equalityKeyOfXmlAttributeOverridesToAvoidXmlSerializerMemoryLeak, XmlAttributeOverrides xmlAttributeOverrides)
         {
-            using (var stream = GetAsStream(assembly, xmlResourceName))
-            {
-                return stream.DeserialiseFromXml<T>(xmlAttributeOverrides);
-            }
+            using var stream = GetAsStream(assembly, xmlResourceName);
+            return stream.DeserialiseFromXml<T>(equalityKeyOfXmlAttributeOverridesToAvoidXmlSerializerMemoryLeak, xmlAttributeOverrides);
         }
     }
 }
